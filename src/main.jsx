e82069f9e3aa13a31eb55af06f1f2ed59b5f10cd
M Zidan Kusuma Firdaus <zidankusumafirdaus.school@gmail.com>
2025-07-17 19:54:22 +0700

    refactor: switch app.jsx to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
+// Importing pages
+import GuestPage from "./pages/guest/GuestPage";
+import QueueNumber from "./pages/guest/QueueNumber";
+import VisitPage from "./pages/cs/GuestVisitPage";
+import LoginPage from "./pages/cs/LoginPage";
+import CSLogs from "./pages/cs/CSLogs";
+import AllGuestPage from "./pages/cs/AllGuestPage";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+      <Route path="/" element={<Navigate to="/guest" replace />} />
+      <Route path="/guest" element={<GuestPage />} />
+      <Route path="/visit" element={<VisitPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/queue-number" element={<QueueNumber />} />
+      <Route path="/cslogs" element={<CSLogs />} />
+      <Route path="/all-guests" element={<AllGuestPage />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
