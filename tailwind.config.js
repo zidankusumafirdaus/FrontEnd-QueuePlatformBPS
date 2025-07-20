75cdf0523d14758cf03fd2cba4499e784621e4a4
M Zidan Kusuma Firdaus <zidankusumafirdaus.school@gmail.com>
2025-07-20 21:22:30 +0700

    croce: update dependencies & add new color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        BiruTerang: "#00AEEF",
+      }
+    },
   },
   plugins: [],
 }
