1100c9bc2a5b667261953ac300b03c52b36548eb
M Zidan Kusuma Firdaus <zidankusumafirdaus.school@gmail.com>
2025-07-25 06:18:04 +0700

    feat: add protect route to Admin Panel

---
+++ src/utils/ProtectRoute.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { isLoggedIn } from "./auth";
+
+const ProtectedRoute = ({ children }) => {
+  console.log("Is Logged In:", isLoggedIn());
+
+  if (!isLoggedIn()) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
