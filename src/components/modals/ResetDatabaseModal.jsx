c10585e7c3382c4b81047b49ec1a35aad3f33984
M Zidan Kusuma Firdaus <zidankusumafirdaus.school@gmail.com>
2025-08-13 14:21:01 +0700

    fix: repair reset visit

--- src/components/modals/ResetDatabaseModal.jsx
+++ src/components/modals/ResetDatabaseModal.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import ConfirmModal from "../guest/ConfirmModal";
+import { resetDatabase } from "../../service/api/api";
+import { toast } from "react-toastify";
 
 const ResetDatabaseModal = ({ show, onClose }) => {
+  const handleConfirm = async () => {
+    try {
+      await resetDatabase();
+      toast.success("Database berhasil direset!", { position: "top-right" });
+    } catch {
+      toast.error("Terjadi kesalahan saat mereset database.", { position: "top-right" });
+    } finally {
+      onClose();
+    }
+  };
+
   return (
     <ConfirmModal
       show={show}
       onClose={onClose}
-      onConfirm={() => {
-        const resetDatabaseBtn = document.querySelector('[data-reset-database-btn]');
-        if (resetDatabaseBtn) {
-          resetDatabaseBtn.click();
-        }
-        onClose();
-      }}
+      onConfirm={handleConfirm}
       message={
         <>
           Yakin ingin menghapus semua data kunjungan dari{" "}
